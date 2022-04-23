1736a7748aee70275cef9b935aedf54592473f81
AnonGhost09 <pramudyaalamsyah@gmail.com>
2022-04-23 09:36:47 +0700

    isi paragraph about

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function index() {
+  return (
+    <section id="about pt-36 pb-32">
+      <div class="container">
+        <p>tes</p>
+        <p>tes</p>
+      </div>
+    </section>
+  );
+}
+
+export default index;
