2d939c65c23c77af399c45362cf1c9e55496551d
AnonGhost09 <pramudyaalamsyah@gmail.com>
2022-04-23 19:35:04 +0700

    About jalan

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,28 @@ import React from "react";
 function index() {
   return (
     <section id="about pt-36 pb-32">
-      <div class="container">
-        <p>tes</p>
-        <p>tes</p>
-        <p>xxxxx</p>
+      <div className="container">
+        <div className="flex flex-wrap">
+          <div className="w-full px-4 mb-10">
+            <h4 className="font-bold uppercase">Tentang Saya</h4>
+            <h2>Yuk, Belajar Web di WPU</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad amet
+              quasi consequatur reiciendis numquam earum nobis dolorum nulla
+              deleniti ipsum molestiae eius, cumque impedit magnam sint iusto
+              distinctio sed? Similique?
+            </p>
+          </div>
+          <div className="w-full px-4">
+            <h3> Mari berteman</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              quidem iste, molestiae voluptatum vel, doloribus impedit quam
+              totam repellendus dignissimos ipsa quas placeat eaque error fugiat
+              itaque est? Pariatur, autem?
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
