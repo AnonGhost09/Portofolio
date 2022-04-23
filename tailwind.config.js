0cb9e663003e500471d2ad26875c539dc93445e9
AnonGhost09 <pramudyaalamsyah@gmail.com>
2022-04-23 09:04:44 +0700

    clear

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
